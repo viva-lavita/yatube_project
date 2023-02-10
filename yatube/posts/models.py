3a6e1ab0cde065e4306ff76b1b4ed86daa8c9cfb
Виктория Сопина <vt.sopina@yandex.ru>
2023-02-11 02:26:07 +0300

    Final version

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,6 +4,15 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(null=False, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -12,3 +21,11 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name='posts'
     )
+    group = models.ForeignKey(
+        'Group', blank=True, null=True,
+        on_delete=models.SET_NULL,
+        related_name='posts'
+    )
+
+    class Meta:
+        ordering = ('-pub_date',)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,22 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
     template = 'posts/index.html'
+    posts = Post.objects.all()[:10]
     title = 'Это главная страница проекта Yatube'
     context = {
-        'title': title
+        'posts': posts,
+        'title': title,
     }
     return render(request, template, context)
 
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    title = 'Здесь будет информация о группах проекта Yatube'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).all()[:10]
+    title = f'Посты сообщества {group}'
     context = {
-        'title': title
+        'group': group,
+        'posts': posts,
+        'title': title,
     }
     return render(request, template, context)
-
-# Create your views here.
